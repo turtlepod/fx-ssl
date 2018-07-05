82029a108aad27bc8f9cae1664f587f8ad4a7cc0
david <david.chandra@10up.com>
2018-07-05 10:12:43 +0700

    Change settings, remove filters, code standard.

--- fx-ssl.php
+++ fx-ssl.php
@@ -3,7 +3,7 @@
  * Plugin Name: f(x) SSL
  * Plugin URI: http://genbu.me/plugins/fx-ssl/
  * Description: Simple SSL(HTTPS) Plugin.
- * Version: 1.1.0
+ * Version: 2.0.0
  * Author: David Chandra Purnama
  * Author URI: http://shellcreeper.com/
  *
@@ -20,13 +20,15 @@
 **/
 
 /* Do not access this file directly */
-if ( ! defined( 'WPINC' ) ) { die; }
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 /* Constants
 ------------------------------------------ */
 
 /* Set the version constant. */
-define( 'FX_SSL_VERSION', '1.1.0' );
+define( 'FX_SSL_VERSION', '2.0.0' );
 
 /* Set the constant path to the plugin path. */
 define( 'FX_SSL_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
@@ -38,86 +40,79 @@ define( 'FX_SSL_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 /* Plugins Loaded
 ------------------------------------------ */
 
-/* Load plugins file */
-add_action( 'plugins_loaded', 'fx_ssl_plugins_loaded' );
-
 /**
  * Load plugins file
+ *
  * @since 0.1.0
  */
-function fx_ssl_plugins_loaded(){
+function fx_ssl_plugins_loaded() {
 
-	/* Language */
+	// Language.
 	load_plugin_textdomain( 'fx-ssl', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-	/* Load Settings */
-	if( is_admin() ){
+	// Load Settings.
+	if( is_admin() ) {
 		require_once( FX_SSL_PATH . 'includes/settings.php' );
 		$fx_ssl_settings = new fx_SSL_Settings();
 	}
 }
+add_action( 'plugins_loaded', 'fx_ssl_plugins_loaded' );
 
-/* Load Functions */
+// Load Functions
 require_once( FX_SSL_PATH . 'includes/functions.php' );
 
 
 /* Activation and Uninstall
 ------------------------------------------ */
 
-/* Register activation hook. */
-register_activation_hook( __FILE__, 'fx_ssl_activation' );
-
-
 /**
+ * Register activation hook.
  * Runs only when the plugin is activated.
+ *
  * @since 0.1.0
  */
 function fx_ssl_activation() {
-
-	/* Add notice. */
 	if ( is_ssl() && force_ssl_admin() && fx_ssl_is_https( get_bloginfo( 'url' ) ) && fx_ssl_is_https( get_bloginfo( 'wpurl' ) ) ) {
 		set_transient( 'fx_ssl_notice', 'success', 5 );
-		if( get_option( 'fx-ssl', false ) ){
+		if( get_option( 'fx-ssl', false ) ) {
 			set_transient( 'fx_ssl_notice', 'active', 5 );
 		}
-	}
-	else{
+	} else {
 		set_transient( 'fx_ssl_notice', 'fail', 5 );
 	}
 }
-
-
-/* Add admin notice */
-add_action( 'admin_notices', 'fx_ssl_admin_notice' );
+register_activation_hook( __FILE__, 'fx_ssl_activation' );
 
 /**
  * Admin Notice on Activation.
+ *
  * @since 0.1.0
  */
-function fx_ssl_admin_notice(){
-	if( 'fail' === get_transient( 'fx_ssl_notice' ) ){
-		?>
-		<div class="updated notice is-dismissible">
-			<?php echo wpautop( apply_filters( 'fx_ssl_notice_fail', sprintf( _x( 'You need to enable (and enforce) <a href="%s" target="_blank">WordPress administration over SSL</a> and use HTTPS URL in "WordPress Address (URL)" and "Site Address (URL)" in <a href="%s">General Settings</a> to use f(x) SSL plugin.', 'activation notice', 'fx-ssl' ), 'https://codex.wordpress.org/Administration_Over_SSL', admin_url( 'options-general.php' ) ) ) );?>
-		</div>
-		<?php
-		delete_transient( 'fx_ssl_notice' );
-	}
-	elseif( 'success' === get_transient( 'fx_ssl_notice' ) ){
-		?>
-		<div class="updated notice is-dismissible">
-			<?php echo wpautop( apply_filters( 'fx_ssl_notice_success', sprintf( _x( 'Navigate to <a href="%s">General Settings</a> to activate SSL (HTTPS) in your site.', 'activation notice', 'fx-ssl' ), admin_url( 'options-general.php' ) ) ) );?>
-		</div>
-		<?php
-		delete_transient( 'fx_ssl_notice' );
-	}
-	elseif( 'active' === get_transient( 'fx_ssl_notice' ) ){
-		?>
-		<div class="updated notice is-dismissible">
-			<?php echo wpautop( apply_filters( 'fx_ssl_notice_active', _x( 'Your site SSL (HTTPS) is active.', 'activation notice', 'fx-ssl' ) ) );?>
-		</div>
-		<?php
-		delete_transient( 'fx_ssl_notice' );
-	}
-}
+function fx_ssl_admin_notice() {
+	?>
 
+	<?php if( 'fail' === get_transient( 'fx_ssl_notice' ) ) : ?>
+
+		<div class="updated notice is-dismissible">
+			<?php echo wpautop( sprintf( _x( 'You need to enable (and enforce) <a href="%s" target="_blank">WordPress administration over SSL</a> and use HTTPS URL in "WordPress Address (URL)" and "Site Address (URL)" in <a href="%s">General Settings</a> to use f(x) SSL plugin.', 'activation notice', 'fx-ssl' ), 'https://codex.wordpress.org/Administration_Over_SSL', admin_url( 'options-general.php' ) ) ); ?>
+		</div>
+		<?php  ?>
+
+	<?php elseif ( 'success' === get_transient( 'fx_ssl_notice' ) ) : ?>
+
+		<div class="updated notice is-dismissible">
+			<?php echo wpautop( 'fx_ssl_notice_success', sprintf( _x( 'Navigate to <a href="%s">General Settings</a> to activate SSL (HTTPS) in your site.', 'activation notice', 'fx-ssl' ), admin_url( 'options-general.php' ) ) ); ?>
+		</div>
+
+	<?php elseif ( 'active' === get_transient( 'fx_ssl_notice' ) ) : ?>
+
+		<div class="updated notice is-dismissible">
+			<?php echo wpautop( _x( 'Your site SSL (HTTPS) is active.', 'activation notice', 'fx-ssl' ) );?>
+		</div>
+
+	<?php endif; ?>
+
+	<?php
+	delete_transient( 'fx_ssl_notice' );
+}
+add_action( 'admin_notices', 'fx_ssl_admin_notice' );
--- includes/settings.php
+++ includes/settings.php
@@ -1,116 +1,114 @@
 <?php
 /**
  * Settings
+ *
  * @since 0.1.0
 **/
-/* Do not access this file directly */
-if ( ! defined( 'WPINC' ) ) { die; }
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 
 /**
  * Create Settings Page
+ *
  * @since 0.1.0
  */
-class fx_SSL_Settings{
+class fx_SSL_Settings {
 
 	/**
 	 * Settings Slug
+	 *
 	 * @since 0.1.0
 	 */
 	public $settings_slug = 'general';
 
 	/**
 	 * Options Group
+	 *
 	 * @since 0.1.0
 	 */
 	public $options_group = 'general';
 
 	/**
 	 * Option Name
+	 *
 	 * @since 0.1.0
 	 */
 	public $option_name = 'fx-ssl';
 
 	/**
 	 * Construct
+	 *
 	 * @since 0.1.0
 	 */
-	public function __construct(){
-
-		/* Register Settings and Fields */
+	public function __construct() {
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
 	/**
 	 * Register Settings
+	 *
 	 * @since 0.1.0
 	 */
-	public function register_settings(){
+	public function register_settings() {
 
-		/* Register settings */
 		register_setting(
-			$this->options_group, // options group
-			$this->option_name, // option name/database
-			array( $this, 'sanitize' ) // sanitize callback function
+			$option_group    = $this->options_group,
+			$option_name     = $this->option_name,
+			$sanitize_cb     = function( $input ) {
+				return isset( $input ) ? true : false;
+			}
 		);
 
-		/* Create settings section */
 		add_settings_section(
-			'fx_ssl_section', // section ID
-			_x( 'SSL (HTTPS)', 'settings page', 'fx-ssl' ), // section title
-			array( $this, 'settings_section' ), // section callback function
-			$this->settings_slug // settings page slug
+			$section_id      = 'fx_ssl_section',
+			$section_title   = esc_html__( 'SSL (HTTPS)', 'fx-ssl' ),
+			$callback        =  function() {
+				?>
+				<?php
+			},
+			$slug            = $this->settings_slug
 		);
 
-		/* Field: Front Page Title */
 		add_settings_field(
-			'fx_ssl', // field ID
-			_x( 'Force SSL', 'settings page', 'fx-ssl' ), // field title 
-			array( $this, 'settings_field_ssl' ), // field callback function
-			$this->settings_slug, // settings page slug
-			'fx_ssl_section' // section ID
+			$field_ID        = 'fx_ssl_info',
+			$field_title     = esc_html__( 'Requirement', 'fx-ssl' ), 
+			$callback        = function() {
+				$yes = '<span class="dashicons dashicons-yes" style="color:green;"></span>';
+				$no  = '<span class="dashicons dashicons-no" style="color:red;"></span>';
+				?>
+				<ul>
+					<li>
+						
+						<p><?php echo is_ssl() && force_ssl_admin() ? $yes : $no; ?> <strong><?php esc_html_e( 'Enforce Administration over SSL.', 'fx-ssl' ); ?></strong></p>
+						<p><?php esc_html_e( 'Add this code in wp-config.php to enable this:', 'fx-ssl' ); ?> <code>define( 'FORCE_SSL_ADMIN', true );</code></p>
+						<p><a target="_blank" href="https://codex.wordpress.org/Administration_Over_SSL"><?php esc_html_e( 'Read more&hellip;', 'fx-ssl' ); ?></a></p>
+					</li>
+				</ul>
+				<?php
+			},
+			$slug            = $this->settings_slug,
+			$section_id      = 'fx_ssl_section'
 		);
 
+		add_settings_field(
+			$field_ID        = 'fx_ssl',
+			$field_title     = esc_html__( 'Force SSL/HTTPS', 'fx-ssl' ), 
+			$callback        = function() {
+				?>
+
+				<?php if ( is_ssl() && force_ssl_admin() && fx_ssl_is_https( get_option( 'home' ) ) && fx_ssl_is_https( get_option( 'siteurl' ) ) ) : ?>
+					<label for="fx_ssl_enable"><input type="checkbox" value="1" id="fx_ssl_enable" name="<?php echo esc_attr( $this->option_name );?>" <?php checked( $option ); ?>> <?php esc_html_e( 'Redirect all pages to HTTPS', 'fx-ssl' ); ?></label>
+				<?php else : ?>
+					<p><?php esc_html_e( 'You need to enforce WordPress administration over SSL and use HTTPS URL in "WordPress Address (URL)" and "Site Address (URL)" to enable this feature.', 'fx-ssl' ); ?></p>
+				<?php endif; ?>
+
+				<?php
+			},
+			$slug            = $this->settings_slug,
+			$section_id      = 'fx_ssl_section'
+		);
 	}
-
-	/**
-	 * Sanitize Options
-	 * @since 0.1.0
-	 */
-	public function sanitize( $input ){
-		return isset( $input ) ? true : false;
-	}
-
-	/**
-	 * Settings Section
-	 * @since 0.1.0
-	 */
-	public function settings_section(){
-		?>
-		<p style="font-size:14px;"><?php echo apply_filters( 'fx_ssl_settings_section_description', sprintf( _x( 'You need to enable (and enforce) <a href="%s" target="_blank">WordPress administration over SSL</a> and use HTTPS URL in "WordPress Address (URL)" and "Site Address (URL)" settings above to activate this feature.', 'settings page', 'fx-ssl' ), 'https://codex.wordpress.org/Administration_Over_SSL' ) );?></p>
-		<?php
-	}
-
-	/**
-	 * Enable Private Site
-	 * @since 0.1.0
-	 */
-	public function settings_field_ssl(){
-
-		/* Check if feature is supported. */
-		if( is_ssl() && force_ssl_admin() && fx_ssl_is_https( get_option( 'home' ) ) && fx_ssl_is_https( get_option( 'siteurl' ) ) ){
-			$disabled = '';
-			$option = get_option( $this->option_name, false );
-		}
-		/* Feature is not supported. */
-		else{
-			$disabled = ' disabled=disabled';
-			$option = false; // always false if requirement not met.
-		}
-	?>
-		<label for="fx_ssl_enable">
-			<input type="checkbox" value="1" id="fx_ssl_enable" name="<?php echo esc_attr( $this->option_name );?>" <?php checked( $option ); ?><?php echo $disabled; ?>> <?php _ex( 'Redirect all pages to HTTPS', 'settings page', 'fx-ssl' );?></label>
-	<?php
-	}
-
 }
